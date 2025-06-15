6c362ebe7480ad424d4837e9a283356d120141e0
Yasuo <131862810+ynagarotest@users.noreply.github.com>
2025-06-16 02:05:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const PORT = process.env.PORT || 8080;
 const REDIRECT_DOMAIN = 'https://ynagaro.com';
 
 app.use((req, res) => {
-    
     const sessionStartTimeUsec = Date.now() * 1000;
     const fullPathWithQuery = req.originalUrl;
     const landingUrl = `${REDIRECT_DOMAIN}${fullPathWithQuery}`;
 
-    // Cloud Loggingは、特定のJSON構造を解釈してリッチなログを生成する
     // https://cloud.google.com/logging/docs/structured-logging
     const logEntry = {
         severity: 'INFO',
@@ -24,14 +22,10 @@ app.use((req, res) => {
         }
     };
 
-    // JSON形式で標準出力する (これがCloud Loggingに記録される)
     console.log(JSON.stringify(logEntry));
-
-    // 301 (Moved Permanently) リダイレクトを実行
     res.redirect(301, destinationUrl);
 });
 
-// サーバーを起動
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
 });
